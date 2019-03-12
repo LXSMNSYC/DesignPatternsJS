fe16f1f1124d758a86c3f99edb9a77f2febe4b9b
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-12 16:24:35 +0800

    Create circle-container.js

---
+++ src/esnext/creational/factory-method/circle-container.js
@@ -0,0 +1,60 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import BoundingContainer from './bounding-container';
+
+/**
+ * @desc
+ *
+ */
+export default class CircleContainer extends BoundingContainer {
+  /**
+   * @desc
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} radius
+   */
+  constructor(x, y, radius) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+  /**
+   * @desc
+   * @param {Number} tx
+   * @param {Number} ty
+   * @return {Boolean}
+   */
+  containsPoint(tx, ty) {
+    const dx = tx - this.x;
+    const dy = ty - this.y;
+    return dx * dx + dy * dy <= this.radius * this.radius;
+  }
+}
