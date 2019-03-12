fba4ffd75e84ed860bbf298961eac724385fa395
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-12 16:24:29 +0800

    Update window.js

--- src/esnext/creational/builder/window.js
+++ src/esnext/creational/builder/window.js
@@ -25,11 +25,11 @@
  * @author Alexis Munsayac <alexis.munsayac@gmail.com>
  * @copyright Alexis Munsayac 2019
  */
- 
+
 /**
  * @desc
- * 
+ *
  */
-export default class Window{
+export default class Window {
 
 }
