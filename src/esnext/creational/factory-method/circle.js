d4732553f436101976682ece3a73d55e0e0392f0
Alexis Munsayac <alexis.munsayac@gmail.com>
2019-03-12 16:24:41 +0800

    Create circle.js

---
+++ src/esnext/creational/factory-method/circle.js
@@ -0,0 +1,56 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2019 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2019
+ */
+import FactoryMethod from './factory-method';
+import CircleContainer from './circle-container';
+/**
+ * @desc
+ */
+export default class Circle extends FactoryMethod {
+  /**
+   * @desc
+   *
+   * @param {Number} x
+   * @param {Number} y
+   * @param {Number} radius
+   */
+  constructor(x, y, radius) {
+    super();
+    this.x = x;
+    this.y = y;
+    this.radius = radius;
+  }
+
+  /**
+   * @desc
+   *
+   * @returns {CircleContainer}
+   */
+  makeContainer() {
+    return new CircleContainer(this.x, this.y, this.radius);
+  }
+}
